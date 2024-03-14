456930d7f6fad71f0249c8297e9c918245312fed
Victor Pozdnyakov <victor.pozdnyakov@techwings.com>
2024-03-14 19:04:09 +0600

    Add paddings

--- app/src/main/java/com/lime/regioncodeskg/ui/navigation/drawer/DrawerContent.kt
+++ app/src/main/java/com/lime/regioncodeskg/ui/navigation/drawer/DrawerContent.kt
@@ -1,12 +1,17 @@
 package com.lime.regioncodeskg.ui.navigation.drawer
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DrawerContent(
     items: List<DrawerItem>,
     onItemClicked: (String) -> Unit
 ) {
+    Spacer(modifier = Modifier.height(22.dp))
     items.forEach { item ->
         DrawerItemComposable(
             item = item,
--- app/src/main/java/com/lime/regioncodeskg/ui/navigation/drawer/DrawerItemComposable.kt
+++ app/src/main/java/com/lime/regioncodeskg/ui/navigation/drawer/DrawerItemComposable.kt
@@ -4,8 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +15,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DrawerItemComposable(
@@ -24,11 +27,17 @@ fun DrawerItemComposable(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onItemClicked(item.route) },
+            .wrapContentHeight()
+            .clickable { onItemClicked(item.route) }
+            .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(imageVector = ImageVector.vectorResource(item.iconResId), contentDescription = null)
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = stringResource(id = item.titleResId), modifier = Modifier.wrapContentWidth())
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            modifier = Modifier.weight(1f),
+            text = stringResource(id = item.titleResId),
+            style = TextStyle(fontSize = 22.sp)
+        )
     }
 }
