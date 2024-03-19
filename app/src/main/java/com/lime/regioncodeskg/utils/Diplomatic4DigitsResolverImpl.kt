04160a2466cbf0173be400b71183abd968daffc9
Victor Pozdnyakov <victor.pozdnyakov@techwings.com>
2024-03-19 22:00:44 +0600

    Add consular and organizations' string resources

--- app/src/main/java/com/lime/regioncodeskg/utils/Diplomatic4DigitsResolverImpl.kt
+++ app/src/main/java/com/lime/regioncodeskg/utils/Diplomatic4DigitsResolverImpl.kt
@@ -14,6 +14,20 @@ class Diplomatic4DigitsResolverImpl(private val resources: Resources) : NumericP
 
         return when(numericInput.joinToString(separator = "")) {
             "1001" -> resources.getString(R.string.latvia)
+            "1101" -> resources.getString(R.string.hungary)
+            "1201" -> resources.getString(R.string.italy)
+            "1301" -> resources.getString(R.string.canada)
+            "1401" -> resources.getString(R.string.sweden)
+            "1501" -> resources.getString(R.string.united_kingdom)
+            "1601" -> resources.getString(R.string.cyprus)
+            "1701" -> resources.getString(R.string.czech_republic)
+            "1801" -> resources.getString(R.string.moldova)
+            "1901" -> resources.getString(R.string.slovak_republic)
+            "2001" -> resources.getString(R.string.romania)
+            "2101" -> resources.getString(R.string.lithuania)
+            "2201" -> resources.getString(R.string.netherlands)
+            "2301" -> resources.getString(R.string.turkey)
+            "2401" -> resources.getString(R.string.belgium)
             else -> resources.getString(R.string.diplomatic_not_defined)
         }
     }
